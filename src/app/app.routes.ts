edf67e96ce0e588ae8eeca8d405606b58560c2cd
Robpiot <robin.piot@outlook.com>
2024-12-08 05:34:29 +0100

    feat: generic pages-routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from '~/app/landing-page/landing-page.component';
+import { HomeComponent } from '~/app/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path : '',
+    component: HomeComponent,
+  },
+  {
+    path : 'landing',
+    component: LandingPageComponent,
+  }
+];
